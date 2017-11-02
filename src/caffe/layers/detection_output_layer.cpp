a63511d2cc9bf63f641a30113ab07e52e29fc1dd
aanokhov <33293658+aanokhov@users.noreply.github.com>
2017-11-02 12:16:25 +0300

    Workaround for SSD (@aanokhov)

--- src/caffe/layers/detection_output_layer.cpp
+++ src/caffe/layers/detection_output_layer.cpp
@@ -215,10 +215,10 @@ void DetectionOutputLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
   top[0]->Reshape(top_shape);
 
   // Patch for MO
-/*  vector<int> top_shape2(2, 1);
+  vector<int> top_shape2(2, 1);
   top_shape2.push_back(keep_top_k_);
   top_shape2.push_back(7);
-  top[0]->Reshape(top_shape2);*/
+  top[0]->Reshape(top_shape2);
 }
 
 template <typename Dtype>
